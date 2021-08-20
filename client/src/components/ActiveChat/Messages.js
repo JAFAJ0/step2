ed65965f4e5949fa6e214645a49324dbce9679b7
JA <aij@mcmaster.ca>
2021-08-20 13:38:05 -0400

    Update Messages.js

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -21,19 +21,30 @@ const useStyles = makeStyles(() => ({
 const Messages = (props) => {
   const { messages, otherUser, userId, otherUserSeen } = props;
   const classes = useStyles();
+  function ous() {
+    let ons = 0;
+    for (var i = otherUserSeen; i > 0; i--) {
+      if (messages[i - 1].senderId === userId) {
+        ons = i;
+        break;
+      }
+    }
+    return ons;
+  }
+  const oUS = ous();
   return (
-    <Box>
+    <Box >
       {messages.map((message, index) => {
         const time = moment(message.createdAt).format("h:mm");
         return (
-          <Box key={message.id}>
+          <Box key={message.id} >
             {message.senderId === userId ? (
               <SenderBubble text={message.text} time={time} />
             ) : (
               <OtherUserBubble text={message.text} time={time} otherUser={otherUser} />
             )}
-            <Box className={classes.root} >
-              {index + 1 === otherUserSeen && <Avatar alt={otherUser.username} src={otherUser.photoUrl} className={classes.avatar}></Avatar>}
+            <Box className={classes.root} mb={8}>
+              {index + 1 === oUS && <Avatar alt={otherUser.username} src={otherUser.photoUrl} className={classes.avatar}></Avatar>}
             </Box>
           </Box>);
       })}
